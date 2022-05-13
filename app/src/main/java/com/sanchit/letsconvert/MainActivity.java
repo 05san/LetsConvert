4c7e2bf8fb905cf1342c978f8b1663342aeed994
05San <sanchit0701@gmail.com>
2022-05-14 02:19:57 +0530

    Lets Start the Conversion

--- app/src/main/java/com/sanchit/letsconvert/MainActivity.java
+++ app/src/main/java/com/sanchit/letsconvert/MainActivity.java
@@ -3,12 +3,35 @@ package com.sanchit.letsconvert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private EditText editNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editNumber = findViewById(R.id.editNumber);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editNumber.getText().toString();
+                int kg= Integer.parseInt(s);
+                double pound= 2.205 * kg;
+                textView.setText("Pound value is " +pound);
+            }
+        });
+
+
     }
 }
